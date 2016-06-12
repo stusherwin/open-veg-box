8680074e1ebf64c1e17037ec3307e06684a54737
Stu Sherwin <stu.sherwin@gmail.com>
2016-06-12 09:14:28 +0100

    Getting ts & less compiling

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,10 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    styleUrls: ['app/app.component.css'],
+    template: `
+<h1>My First Angular 2 STU dfs</h1>
+<my-stu></my-stu>
+`
 })
 export class AppComponent { }
---
+++ app/menu.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-stu',
+    styleUrls: ['app/menu.component.css'],
+    template: `
+<h2>Stu 1 hi there sddff</h2>
+`
+})
+export class MenuComponent { } 
