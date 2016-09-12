6dff7ce952bc2785b08afbd3f58b412949fef396
Stu Sherwin <stu.sherwin@gmail.com>
2016-09-12 08:42:45 +0100

    logout in nav

--- client/app/header/header.component.ts
+++ client/app/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './nav-bar.component';
 import { ROUTER_DIRECTIVES, Router } from '@angular/router-deprecated';
+import { LogoutComponent } from '../auth/logout.component'
 
 @Component({
   selector: 'cc-header',
   styleUrls: ['app/header/header.component.css'],
   templateUrl: 'app/header/header.component.html',
-  directives: [NavBarComponent, ROUTER_DIRECTIVES]
+  directives: [NavBarComponent, LogoutComponent, ROUTER_DIRECTIVES]
 })
 
 export class HeaderComponent {
