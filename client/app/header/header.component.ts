71ff27e6c6e07fc64e98f13519bc43035430ca2c
Stu Sherwin <stu.sherwin@gmail.com>
2016-09-26 08:59:55 +0100

    wip

--- client/app/header/header.component.ts
+++ client/app/header/header.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from './nav-bar.component';
 import { ROUTER_DIRECTIVES, Router } from '@angular/router-deprecated';
 import { LogoutComponent } from '../auth/logout.component'
+import { LogoComponent } from './logo.component'
 
 @Component({
   selector: 'cc-header',
   styleUrls: ['app/header/header.component.css'],
   templateUrl: 'app/header/header.component.html',
-  directives: [NavBarComponent, LogoutComponent, ROUTER_DIRECTIVES]
+  directives: [NavBarComponent, LogoutComponent, ROUTER_DIRECTIVES, LogoComponent]
 })
 
 export class HeaderComponent {
---
+++ client/app/header/logo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cc-logo',
+  styleUrls: ['app/header/logo.component.css'],
+  templateUrl: 'app/header/logo.component.html',
+})
+
+export class LogoComponent {
+}
