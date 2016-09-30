2dda111b0729ef84c725f74864b6e9e2fd80ede3
Stu Sherwin <stu.sherwin@gmail.com>
2016-09-30 09:32:19 +0100

    Clean up / split up styles

--- client/app/collection-points/collection-points.component.ts
+++ client/app/collection-points/collection-points.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'cc-products',
-  template: '<div class="container"><div class="container2"><h2>Collection Points</h2><p>Nothing to see here yet...</p><div class="after"></div></div></div>'
+  template: '<div class="page-outer"><div class="page-inner"><h2>Collection Points</h2><p>Nothing to see here yet...</p></div></div>'
 })
 
 export class CollectionPointsComponent { } 
