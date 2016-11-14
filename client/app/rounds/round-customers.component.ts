ab25016ebb1bdf5a66352bf5ca776b2eda2f53cf
Stu Sherwin <stu.sherwin@gmail.com>
2016-11-14 12:01:27 +0000

    Round Customers alternative UI

--- client/app/rounds/round-customers.component.ts
+++ client/app/rounds/round-customers.component.ts
@@ -6,33 +6,10 @@ import { RoundCustomer } from './round'
 @Component({
   selector: 'cc-round-customers',
   directives: [FocusDirective, HighlightableDirective],
-  template: `
-    <div class="round-customers">
-      <ul *ngIf="value.length">
-        <li *ngFor="let c of value">{{c.name}} <a href="#" cc-focus highlight="true" [tabindex]="editTabindex" (click)="$event.preventDefault();removeCustomerClick(c.id)">remove</a></li>
-      </ul>
-      <select class="input" cc-focus highlight="true" [tabindex]="editTabindex" (focus)="onChildFocus()" (blur)="addCustomerBlur();onChildBlur()" [(ngModel)]="customerIdToAdd" (ngModelChange)="addCustomerModelChange($event)" (click)="addCustomerClick($event)">
-        <option [ngValue]="0">Add a customer</option>
-        <option *ngFor="let c of customers" [ngValue]="c.id">{{ c.name }}</option>
-      </select>
-    </div>
-  `
+  templateUrl: 'app/rounds/round-customers.component.html'
 })
 export class RoundCustomersComponent {
-  shouldBlur: boolean;
-  customerIdToAdd: number = 0;
-  
-  @ViewChild('priceElem')
-  priceElem: ElementRef;
-
-  @ViewChild('unitTypeElem')
-  unitTypeElem: ElementRef;
-
-  @Input()
-  editing: boolean;
-
-  @Input()
-  addMode: boolean;
+  focusedCustomerId: number;
 
   @Input()
   editTabindex: number;
@@ -57,83 +34,35 @@ export class RoundCustomersComponent {
 
   @Output()
   remove = new EventEmitter<number>();
-  
-  startEdit() {
-    this.focus.emit({type: "focus", srcElement: this});
-    this.editing = true;
-  }
 
-  endEdit() {
-    this.editing = false;
-    this.blur.emit({type: "blur", srcElement: this});
-  }
-
-  onChildFocus(event: FocusEvent) {
-    this.shouldBlur = false;
-  }
-
-  onChildBlur(event:FocusEvent) {
-    this.shouldBlur = true;
-    setTimeout(() => {
-      if(this.shouldBlur) {
-        this.endEdit();
-        this.shouldBlur = true;
-      }
-    }, 100);
-  }
-
-  isOnRound(customer: RoundCustomer) {
-    return this.value.map(c => c.id).indexOf(customer.id) >= 0;
-  }
-
-  setOnRound(customer: RoundCustomer, evt: any) {
-    if( evt.target.checked ) {
-      this.value.push(customer);
-      this.add.emit(customer.id);
-    } else {
-      let index = this.value.findIndex( c => c.id == customer.id);
-      if(index >= 0) {
-        this.value.splice(index, 1);
-        this.remove.emit(customer.id);
-      }
-    }
-
-    this.valueChange.emit(this.value);
-  }
-  
   removeCustomerClick(customerId: number) {
     let index = this.value.findIndex( c => c.id == customerId);
     if(index >= 0) {
+      let customer = this.value.find( c => c.id == customerId);
       this.value.splice(index, 1);
+      this.customers.push(customer);
       this.remove.emit(customerId);
-    }
-  }
-  
-  addCustomerModelChange(customerId: any) {
-    if(this.clicked) {
-      let customer = this.customers.find( c => c.id == this.customerIdToAdd);
-      if(customer) {
-        this.customerIdToAdd = 0;      
-        this.value.push(customer);
-        this.add.emit(customer.id);
-      }
+      this.focusedCustomerId = null;
     }
   }
 
-  clicked: boolean;
-  addCustomerClick() {
-    this.clicked = true;
+  addCustomerClick(customerId: number ) {
+    let customer = this.customers.find( c => c.id == customerId);
+    if(customer) {
+      let index = this.customers.findIndex( c => c.id == customerId);
+      this.adding = false;
+      this.value.push(customer);
+      this.customers.splice(index, 1);
+      this.add.emit(customer.id);
+      this.focusedCustomerId = null;
+    }
   }
 
-  addCustomerBlur() {
-    if(!this.clicked) {
-      let customer = this.customers.find( c => c.id == this.customerIdToAdd);
-      if(customer) {
-        this.customerIdToAdd = 0;      
-        this.value.push(customer);
-        this.add.emit(customer.id);
-      } 
-    }
-    this.clicked = false;
+  focusCustomer(customerId: number) {
+    this.focusedCustomerId = customerId;
+  }
+
+  unfocusCustomer() {
+    this.focusedCustomerId = null;
   }
 }
