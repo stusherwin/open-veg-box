7616026fc11468f75a18e2c80e4235512d19a988
Stu Sherwin <stu.sherwin@gmail.com>
2017-02-12 19:02:08 +0000

    Refactor box-price to use editable-value

--- client/app/boxes/box-price.component.ts
+++ client/app/boxes/box-price.component.ts
@@ -1,33 +1,30 @@
 import { Component, Input, ViewChild, ElementRef, Output, EventEmitter, OnInit, AfterViewInit, Renderer } from '@angular/core';
 import { MoneyPipe } from '../shared/pipes';
 import { ActiveElementDirective, ActivateOnFocusDirective } from '../shared/active-elements'
+import { EditableValueComponent } from '../shared/editable-value.component'
+import { ValidatableComponent } from '../shared/validatable.component';
 
 @Component({
   selector: 'cc-box-price',
-  directives: [ActiveElementDirective, ActivateOnFocusDirective],
+  directives: [ActiveElementDirective, ActivateOnFocusDirective, EditableValueComponent, ValidatableComponent],
   pipes: [MoneyPipe],
   template: `
-    <div class="x-product-price editable-value" [class.editing]="editing" (keydown)="onKeyDown($event)" #active=cc-active cc-active (deactivate)="onDeactivate()">
-      <div class="editable-value-display" (click)="onClick()">
+    <cc-editable-value #editable className="x-product-price" [valid]="valid" (start)="onStart()" (ok)="onOk()" (cancel)="onCancel()">
+      <display>
         <span class [innerHTML]="value | money"></span>
         <a><i class="icon-edit"></i></a>
-      </div>
-      <div class="editable-value-outer">
-        <div class="editable-value-edit" [class.invalid]="!valid">
-          &pound; <span class="input-wrapper" [class.invalid]="!valid"><input type="text" #input class="input price" data-validation-message="Price should be a number greater than 0" [(ngModel)]="editingValue" (ngModelChange)="validate()" [tabindex]="editTabindex" (focus)="onFocus()" cc-active cc-activate-on-focus />
-          <i *ngIf="!valid" class="icon-warning" title="Price should be a number greater than 0"></i></span>
-          <a (click)="onOkClick()"><i class="icon-ok"></i></a>
-          <a (click)="onCancelClick()"><i class="icon-cancel"></i></a>
-        </div>
-      </div>
-    </div>
+      </display>
+      <edit>
+        &pound;
+        <cc-validatable [valid]="valid" message="Price should be a number greater than 0">
+          <input type="text" #input class="input price" [(ngModel)]="editingValue" [tabindex]="editTabindex" (focus)="startEdit()" cc-active cc-activate-on-focus />
+        </cc-validatable>
+      </edit>
+    </cc-editable-value>
   `
 }) 
-// TODO: active based on focus/blur of child components
 export class BoxPriceComponent implements OnInit, AfterViewInit {
   editingValue: string;
-  editing = false;
-  valid = true;
 
   @Input()
   value: number;
@@ -35,14 +32,14 @@ export class BoxPriceComponent implements OnInit, AfterViewInit {
   @Input()
   editTabindex: number;
 
-  @Input()
-  addMode: boolean;
-
   @ViewChild('input')
   input: ElementRef;
 
   @ViewChild('active')
   active: ActiveElementDirective;
+
+  @ViewChild('editable')
+  editable: EditableValueComponent
 
   @Output()
   valueChange = new EventEmitter<number>();
@@ -50,84 +47,42 @@ export class BoxPriceComponent implements OnInit, AfterViewInit {
   @Output()
   update = new EventEmitter<any>();
 
+  get valid() {
+    return this.toDecimalValue(this.editingValue) > 0;
+  }
+
   constructor(private renderer: Renderer) {
   }
 
   ngOnInit() {
+    this.editingValue = this.toStringValue(this.value);
   }
 
   ngAfterViewInit() {
   }
 
   startEdit() {
-    this.onClick();
+    this.editable.startEdit();
   }
 
-  onFocus() {
-    if(this.editing) {
-      return;
-    }
-
-    this.onClick();
-  }
-
-  onClick() {
-    this.editingValue = this.toStringValue(this.value);
-    this.editing = true;
-    this.valid = true;
-    
+  onStart() {
     setTimeout(() => this.renderer.invokeElementMethod(this.input.nativeElement, 'focus', []))
   }
 
-  onOkClick() {
-    if(!this.valid) {
-      return;
-    }
-
+  onOk() {
     this.value = this.toDecimalValue(this.editingValue);
     
     //TODO: just one event!
     this.valueChange.emit(this.value);
     this.update.emit(null);
     
-    this.editing = false;
-    this.tabbedAway = false;
-    this.active.makeInactive()
+    this.editingValue = this.toStringValue(this.value);
+    this.editable.endEdit();
   }
 
-  onCancelClick() {
-    this.editing = false;
-    this.tabbedAway = false;
-    this.active.makeInactive()
-  }
-
-  tabbedAway = false;
-  onKeyDown(event: KeyboardEvent) {
-    if(!this.editing) {
-      return;
-    }
-
-    if(event.key == 'Enter' && this.valid) {
-      this.onOkClick();
-    } else if(event.key == 'Escape') {
-      this.onCancelClick();
-    } else if(event.key == 'Tab') {
-      this.tabbedAway = !event.shiftKey;
-    }
-  }
-
-  onDeactivate() {
-    if(this.editing) {
-      if(this.tabbedAway && this.valid) {
-        this.onOkClick();
-      } else {
-        this.onCancelClick();
-      }
-    }
-  }
-
-  validate() {
-    this.valid = this.toDecimalValue(this.editingValue) > 0;
+  onCancel() {
+    this.editingValue = this.toStringValue(this.value);
+    this.editable.endEdit();
   }
 
   fixedDecimals: number = 2;
