c609a75bc91c579abedb759e47d554ee7a96cf8d
Stu Sherwin <stu.sherwin@gmail.com>
2017-02-17 11:03:54 +0000

    Make email subject/body mandatory

--- client/app/email/send-email.component.ts
+++ client/app/email/send-email.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, Input, AfterViewInit, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { ROUTER_DIRECTIVES, Router } from '@angular/router-deprecated';
 import { FocusDirective } from '../shared/focus.directive';
 import { FocusService } from '../shared/focus.service';
 import { EmailService, EmailMessage, EmailRecipient } from '../email/email.service';
+import { ValidatableComponent } from '../shared/validatable.component';
 
 @Component({
   selector: 'cc-send-email',
   templateUrl: 'app/email/send-email.component.html',
   providers: [FocusService, EmailService],
-  directives: [FocusDirective, ROUTER_DIRECTIVES]
+directives: [FocusDirective, ROUTER_DIRECTIVES, ValidatableComponent]
 })
 export class SendEmailComponent implements AfterViewInit {
   constructor(private emailService: EmailService, private router: Router) {
@@ -17,8 +18,8 @@ export class SendEmailComponent implements AfterViewInit {
   @Input()
   recipients: EmailRecipient[]
 
-  subject: string;
-  body: string;
+  subject: string = '';
+  body: string = '';
 
   @Input()
   cancelLinkParams: any[]
@@ -29,12 +30,26 @@ export class SendEmailComponent implements AfterViewInit {
   @ViewChild('focusable')
   focusable: FocusDirective
 
+  @ViewChildren(ValidatableComponent)
+  validatables: QueryList<ValidatableComponent>
+
   ngAfterViewInit() {
     this.focusable.beFocused();
   }
 
+  validated = false;
+  get valid() {
+    return !this.validated
+      || !this.validatables
+      || !this.validatables.length
+      || this.validatables.toArray().every(v => v.valid);
+  }
+
   send() {
-    this.emailService.send(new EmailMessage(this.recipients, this.subject, this.body))
-                     .subscribe(() => this.router.navigate(this.successLinkParams));
+    this.validated = true;
+    if(this.valid) {
+      this.emailService.send(new EmailMessage(this.recipients, this.subject, this.body))
+                       .subscribe(() => this.router.navigate(this.successLinkParams));
+    }
   }
 }
