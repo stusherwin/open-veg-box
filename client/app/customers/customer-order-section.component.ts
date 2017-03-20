cfde7c86e36ab081f26989b92efaf5854e83040a
Stu Sherwin <stu.sherwin@gmail.com>
2017-03-20 16:34:32 +0000

    live update order totals, combine section headers

---
+++ client/app/customers/customer-order-section.component.ts
@@ -0,0 +1,114 @@
+import { Component, Input, Output, EventEmitter, ViewChild, forwardRef, Inject, ElementRef, OnInit, Renderer, ViewChildren, QueryList } from '@angular/core';
+import { ActiveElementDirective, ActivateOnFocusDirective, DeactivateOnBlurDirective } from '../shared/active-elements'
+import { DistributeWidthDirective, DistributeWidthSumDirective } from './distribute-width.directive'
+import { BoxProductQuantityComponent } from './box-product-quantity.component'
+import { EditableValueComponent } from '../shared/editable-value.component'
+import { Arrays } from '../shared/arrays'
+import { NumericDirective } from '../shared/numeric.directive'
+import { MoneyPipe } from '../shared/pipes'
+import { CustomerOrderSectionModel, CustomerOrderAvailableItem } from './customer-order.component'
+import { CustomerOrderItemModel } from './customers-home.component'
+
+@Component({
+  selector: 'cc-customer-order-section',
+  templateUrl: 'app/customers/customer-order-section.component.html',
+  directives: [ActiveElementDirective, ActivateOnFocusDirective, DeactivateOnBlurDirective, DistributeWidthDirective, BoxProductQuantityComponent, DistributeWidthSumDirective, EditableValueComponent, NumericDirective],
+  pipes: [MoneyPipe]
+})
+export class CustomerOrderSectionComponent implements OnInit {
+  orderItemPadding = 10;
+  quantity: number = 1;
+  addingItem: AddingItem;
+  itemNameArticle: string;
+
+  @Input()
+  model: CustomerOrderSectionModel;
+
+  @Input()
+  tabindex: number;
+
+  @Input()
+  heading: string;
+
+  @Input()
+  itemName: string;
+
+  @ViewChild('select')
+  select: ElementRef;
+
+  @ViewChild('editable')
+  editable: EditableValueComponent
+
+  @Output()
+  addTotalChange = new EventEmitter<number>()
+
+  constructor(private renderer: Renderer) {
+  }
+
+  ngOnInit() {
+    this.itemNameArticle = /^[aeiou]/i.test(this.itemName) ? 'an' : 'a';
+    let item = this.model.itemsAvailable[0];
+
+    this.addingItem = {
+      id: item.id,
+      price: item.price,
+      quantity: 1,
+      unitType: item.unitType
+    };
+  }
+
+  onOrderItemRemove(item: CustomerOrderItemModel, keyboard: boolean) {
+    item.delete();
+    Arrays.remove(this.model.items, item);
+  }
+
+  onAddStart() {
+    this.renderer.invokeElementMethod(this.select.nativeElement, 'focus', []);
+    this.addTotalChange.emit(this.addingItem.price * this.addingItem.quantity);
+  }
+
+  onItemChange(itemId: number) {
+    let item = this.model.itemsAvailable.find(p => p.id == itemId);
+    
+    this.addingItem.id = item.id;
+    this.addingItem.price = item.price;
+    this.addingItem.unitType = item.unitType;
+    this.addTotalChange.emit(this.addingItem.price * this.addingItem.quantity);    
+  }
+
+  onAddOk() {
+    this.model.addItem(this.addingItem.id, this.addingItem.quantity);
+    this.editable.endEdit();
+
+    let item = this.model.itemsAvailable[0];
+    this.addingItem.id = item.id;
+    this.addingItem.price = item.price;
+    this.addingItem.quantity = 1;
+    this.addingItem.unitType = item.unitType;
+    this.addTotalChange.emit(0);
+  }
+
+  onAddCancel() {
+    this.editable.endEdit();
+
+    let item = this.model.itemsAvailable[0];
+    this.addingItem.id = item.id;
+    this.addingItem.price = item.price;
+    this.addingItem.quantity = 1;
+    this.addingItem.unitType = item.unitType;
+    this.addTotalChange.emit(0);
+  }
+
+  onAddingItemQuantityChange(quantity: number) {
+    if(this.editable.editing) {
+      this.addTotalChange.emit(this.addingItem.price * quantity);
+    }
+  }
+}
+
+class AddingItem {
+  id: number;
+  price: number;
+  quantity: number;
+  unitType: string;
+}
--- client/app/customers/customer-order.component.ts
+++ client/app/customers/customer-order.component.ts
@@ -1,23 +1,59 @@
 import { Component, Input, Output, EventEmitter, ViewChild, forwardRef, Inject, ElementRef, OnInit, Renderer, ViewChildren, QueryList } from '@angular/core';
 import { ActiveElementDirective } from '../shared/active-elements'
 import { DistributeWidthDirective, DistributeWidthSumDirective } from './distribute-width.directive'
-import { CustomerOrderModel } from './customers-home.component'
-import { CustomerOrderBoxesComponent } from './customer-order-boxes.component'
-import { CustomerOrderProductsComponent } from './customer-order-products.component'
+import { CustomerOrderModel, CustomerOrderItemModel } from './customers-home.component'
+import { CustomerOrderSectionComponent } from './customer-order-section.component'
 import { MoneyPipe } from '../shared/pipes'
+import { Product } from '../products/product'
+import { Box } from '../boxes/box'
 
 @Component({
   selector: 'cc-customer-order',
   templateUrl: 'app/customers/customer-order.component.html',
-  directives: [ActiveElementDirective, DistributeWidthDirective, DistributeWidthSumDirective, CustomerOrderBoxesComponent, CustomerOrderProductsComponent],
+  directives: [ActiveElementDirective, DistributeWidthDirective, DistributeWidthSumDirective, CustomerOrderSectionComponent],
   pipes: [MoneyPipe]
 })
-export class CustomerOrderComponent {
+export class CustomerOrderComponent implements OnInit {
   orderItemPadding = 10;
+  addTotal = 0;
+  boxesModel: CustomerOrderSectionModel;
+  productsModel: CustomerOrderSectionModel;
 
   @Input()
   model: CustomerOrderModel;
 
   @Input()
   tabindex: number;
+
+  ngOnInit() {
+    this.productsModel = {
+      itemsAvailable: this.model.extraProductsAvailable,
+      items: this.model.extraProducts,
+      addItem: this.model.addProduct
+    }
+
+    this.boxesModel = {
+      itemsAvailable: this.model.boxesAvailable.map(b => ({
+        id: b.id,
+        name: b.name,
+        price: b.price,
+        unitType: 'each'
+      })),
+      items: this.model.boxes,
+      addItem: this.model.addBox
+    }
+  }
+}
+
+export class CustomerOrderSectionModel {
+  itemsAvailable: CustomerOrderAvailableItem[]
+  items: CustomerOrderItemModel[]
+  addItem: (id: number, quantity: number) => void;
+}
+
+export interface CustomerOrderAvailableItem {
+  id: number;
+  price: number;
+  name: string;
+  unitType: string;
 }
